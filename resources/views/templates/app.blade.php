ab32130f9c2f386e0b12da7a703235c15ba925d5
Cyril Gouverneur <cyril@wifibridge.home>
2018-05-09 12:24:16 +0200

    Add Socials Link in footer

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -12,6 +12,9 @@
     <!-- Google Font Quicksand -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500,700" rel="stylesheet">
 
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
     <!-- Main Stylesheet -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="footer-logo">
             <p><span class="blue">La</span> French <span class="red">Coin</span></p>
+            <p class="socials"><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></p>
             <p>Crée par <a target="_blank" href="http://www.cyrilgouverneur.com">Cyril Gouverneur</a></p>
             <p>Copyright &copy; {{ date('Y') }}</p>
             <p>Tous droits réservés</p>
