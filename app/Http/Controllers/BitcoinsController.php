8e903377ff45f7bca52757ed70c6464e0de4af5d
Cyril Gouverneur <cyril@wifibridge.home>
2018-05-10 19:41:13 +0200

    Return all bitcoin accepted shops in admin view

--- app/Http/Controllers/BitcoinsController.php
+++ app/Http/Controllers/BitcoinsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bitcoin;
 
 class BitcoinsController extends Controller
 {
@@ -13,7 +14,9 @@ class BitcoinsController extends Controller
      */
     public function index()
     {
-        //
+        $bitcoins = Bitcoin::orderBy('created_at', 'desc')->get();
+
+        return view('admin', compact('bitcoins'));
     }
 
     /**
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,6 +8,23 @@
                     <div class="card-header">Administration</div>
     
                     <div class="card-body">
+                        <h2>Liste des boutiques</h2>
+                        @if(count($bitcoins) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Nom :</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach($bitcoins as $bitcoin)
+                                    <tr>
+                                        <td>{{ $bitcoin->name }}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>Votre base de donnée est vide</p>
+                        @endif
                     </div>
                 </div>
             </div>
