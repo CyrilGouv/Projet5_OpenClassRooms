ba427c1311adaeff209de28b070ea1234572ea67
Cyril Gouverneur <cyril@wifibridge.home>
2018-05-17 15:20:07 +0200

    Create sidebar templates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -46,24 +46,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 more-right" aos-animate data-aos="fade-left">
-                <div class="more-widget">
-                    <h4>Cours en temps réel</h4>
-                    <p><strong>BTC:</strong> € <span class="btcPrice">7694.46</span></p>
-                    <p><strong>ETH:</strong> € <span class="ethPrice">617.97</span></p>
-                    <p><strong>LTC:</strong> € <span class="ltcPrice">134.87</span></p>
-                </div>
-                <div class="more-widget">
-                    <h4>Dons Bitcoin</h4>
-                    <p>
-                        Nous acceptons les dons volontiers, ceux-ci nous aident à passer plus de temps sur l'ajoût de nouvelles boutiques et pour la démocratisation des cryptomonnaies.
-                    </p>
-                    <p><strong>BTC :</strong><br> 1CTATqw9hs78ARVGWiEZbpkZrWUcGyk9hE</p>
-                    <p><strong>ETH :</strong><br> 0xC58c0089F15dE67a089326C8FEE95EB914C2b3F1</p>
-                    <p><strong>LTC :</strong><br> LXDJjh41AGKMYTydGix1JmYj2M71sH44qp</p>
-                    </p>
-                </div>
-            </div>
+            @include('templates.sidebar')
         </div>
     </div>
 
---
+++ resources/views/templates/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="col-lg-4 more-right" aos-animate data-aos="fade-left">
+    <div class="more-widget">
+        <h4>Cours en temps réel</h4>
+        <p><strong>BTC:</strong> € <span class="btcPrice"></span></p>
+        <p><strong>ETH:</strong> € <span class="ethPrice"></span></p>
+        <p><strong>LTC:</strong> € <span class="ltcPrice"></span></p>
+    </div>
+    <div class="more-widget">
+        <h4>Dons Bitcoin</h4>
+        <p>
+            Nous acceptons les dons volontiers, ceux-ci nous aident à passer plus de temps sur l'ajoût de nouvelles boutiques et pour la démocratisation des cryptomonnaies.
+        </p>
+        <p><strong>BTC :</strong><br> 1CTATqw9hs78ARVGWiEZbpkZrWUcGyk9hE</p>
+        <p><strong>ETH :</strong><br> 0xC58c0089F15dE67a089326C8FEE95EB914C2b3F1</p>
+        <p><strong>LTC :</strong><br> LXDJjh41AGKMYTydGix1JmYj2M71sH44qp</p>
+        </p>
+    </div>
+</div>
